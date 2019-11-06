9a3b91bab709fe1af58c1567709d7c8f1879fa64
Suranart Niamcome <suranartnc@gmail.com>
2019-11-06 14:59:45 +0700

    Add styles to login page

--- src/components/login/index.js
+++ src/components/login/index.js
@@ -1,8 +1,28 @@
 import React from 'react'
 
+import { Flex, Box } from '@grid'
 import withPage from '@lib/page/withPage'
+import { getStatic } from '@lib/static'
 
 import { signIn } from '@features/_auth'
+
+const logoStyle = {
+  display: 'block',
+  paddingBottom: '30px',
+  margin: 'auto',
+  maxWidth: '500px',
+}
+
+const loginButton = {
+  backgroundColor: '#1ed761 !important',
+  borderColor: '#1ed761 !important',
+  padding: '10px 50px',
+  borderRadius: '30px',
+  fontSize: '24px',
+  fontWeight: 'bold',
+  color: 'white',
+  cursor: 'pointer',
+}
 
 function LoginPage() {
   const login = e => {
@@ -11,10 +31,21 @@ function LoginPage() {
   }
 
   return (
-    <form onSubmit={login}>
-      <button>Log in</button>
-    </form>
+    <Flex flexWrap="wrap" justifyContent="center">
+      <Box width={1}>
+        <img
+          css={logoStyle}
+          src={getStatic('images/spotifyLogo.png')}
+          alt={'SpotifyLogo'}
+        />
+      </Box>
+      <Box width={1} style={{ textAlign: 'center' }}>
+        <button css={loginButton} onClick={login}>
+          Login
+        </button>
+      </Box>
+    </Flex>
   )
 }
 
-export default withPage()(LoginPage)
+export default withPage({ layout: false })(LoginPage)
