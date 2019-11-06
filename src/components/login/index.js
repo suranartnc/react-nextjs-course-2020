f70f22c59ee8e218ce5c461cc63b1593a0c84fd2
Suranart Niamcome <suranartnc@gmail.com>
2019-11-06 14:45:47 +0700

    Authenticate using spotify service

--- src/components/login/index.js
+++ src/components/login/index.js
@@ -1,48 +1,20 @@
-import React, { useState } from 'react'
-import { flowRight as compose } from 'lodash'
+import React from 'react'
 
-import { Router } from '@router'
-import { inject } from '@lib/store'
 import withPage from '@lib/page/withPage'
 
 import { signIn } from '@features/_auth'
 
-function LoginPage({ errorStore }) {
-  const [email, setEmail] = useState('')
-  const [password, setPassword] = useState('')
-
+function LoginPage() {
   const login = e => {
     e.preventDefault()
-
-    const { redirect } = Router.router.query
-
-    signIn({ email, password, redirect }).catch(error => {
-      errorStore.addError({
-        title: error.message,
-      })
-    })
+    signIn()
   }
 
   return (
     <form onSubmit={login}>
-      <p>
-        <label>
-          Email:
-          <input type="text" onChange={e => setEmail(e.target.value)} />
-        </label>
-      </p>
-      <p>
-        <label>
-          Password:
-          <input type="password" onChange={e => setPassword(e.target.value)} />
-        </label>
-      </p>
       <button>Log in</button>
     </form>
   )
 }
 
-export default compose(
-  withPage(),
-  inject('errorStore', { observe: false }),
-)(LoginPage)
+export default withPage()(LoginPage)
