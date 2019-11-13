469abbd9994b1e873c1e4ecc0db9c2ff00e23295
Suranart Niamcome <suranartnc@gmail.com>
2019-11-13 19:10:22 +0700

    Prepare code for workshop about state, event and effect

--- src/app.js
+++ src/app.js
@@ -1,5 +1,31 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
+
+const apiUrl = 'https://api.github.com/repos/facebook/react'
 
 export default function App() {
-  return <div>Rendered by React using JSX</div>
+  const [loading, setLoading] = useState(false)
+
+  const fetchData = () => {
+    axios.get(apiUrl).then(response => {
+      console.log(response.data)
+    })
+  }
+
+  if (loading === true) {
+    return 'Loading...'
+  }
+
+  return (
+    <div>
+      <p>Name: ???</p>
+      <p>Stars: ???</p>
+      <button
+        onClick={() => {
+          console.log('Button Clicked')
+        }}>
+        Refresh
+      </button>
+    </div>
+  )
 }
