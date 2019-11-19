fe319e6b5669535268a1ad709c2a95dac69b5513
Suranart Niamcome <suranartnc@gmail.com>
2019-11-19 20:10:13 +0700

    Add more scaffolding

--- src/features/dictionary/index.js
+++ src/features/dictionary/index.js
@@ -1,19 +1,43 @@
-import React from 'react'
-import styles from './DictStyles'
+import React, { useState, useEffect } from "react";
+import styles from "./DictStyles";
+
+import dict from "./data/eng2thai.json";
+
+function translate(word) {
+  return dict.filter(vocab => {
+    return vocab.search === word
+  })
+}
 
 export default function DictApp() {
+  const [results, setResults] = useState([]);
+
+  function onInputChanged(e) {
+    // Write some code here...
+  }
+
+  function onSubmitted() {
+    // Write some code here...
+  }
+
   return (
     <div style={styles.app}>
       <div style={styles.wrapper}>
         <div style={styles.form}>
-          <input style={styles.keyword} placeholder="keywords..." />
-          <button style={styles.button}>Search</button>
+          <input
+            style={styles.keyword}
+            placeholder="keywords..."
+            value=""
+            onChange={onInputChanged}
+          />
+          <button style={styles.button} onClick={onSubmitted}>Search</button>
         </div>
         <ul style={styles.results}>
-          <li>คำแปล 1</li>
-          <li>คำแปล 2 (ถ้ามี)</li>
+          {results.map(function(vocab, index) {
+            return <li key={`${vocab.result}-${index}`}>{vocab.result}</li>;
+          })}
         </ul>
       </div>
     </div>
-  )
+  );
 }
