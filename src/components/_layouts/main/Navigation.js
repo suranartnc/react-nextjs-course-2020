840e6b3ee7d9ff13424a4cf5a8fd49dd545cd906
Suranart Niamcome <suranartnc@gmail.com>
2019-11-22 11:34:06 +0700

    Add navigation in main layout

---
+++ src/components/_layouts/main/Navigation.js
@@ -0,0 +1,38 @@
+import React, { forwardRef } from 'react'
+import Link from '@link'
+import { media } from '@lib/styles'
+
+const mainMenus = [
+  {
+    name: 'Home',
+    route: 'home',
+  },
+]
+
+const LinkItem = forwardRef((props, ref) => (
+  <a
+    {...props}
+    ref={ref}
+    css={{
+      display: 'inline-box',
+      padding: '5px 15px 10px 0px',
+      marginRight: 50,
+      [media('md')]: {
+        fontSize: '1.2em',
+      },
+    }}>
+    {props.menu.name}
+  </a>
+))
+
+export default function Navigation() {
+  return (
+    <nav css={{ marginBottom: 10, borderBottom: '1px solid #aaa' }}>
+      {mainMenus.map(menu => (
+        <Link key={menu.name} route={menu.route} passHref>
+          <LinkItem menu={menu} />
+        </Link>
+      ))}
+    </nav>
+  )
+}
--- src/components/_layouts/main/index.js
+++ src/components/_layouts/main/index.js
@@ -2,6 +2,8 @@ import React from 'react'
 import { ThemeProvider } from 'emotion-theming'
 import { breakpoints } from '@lib/styles'
 
+import Navigation from './Navigation'
+
 const theme = {
   breakpoints: Object.keys(breakpoints).map(key => breakpoints[key]),
 }
@@ -10,6 +12,7 @@ export default function MainLayout({ children, breadcrumb }) {
   return (
     <ThemeProvider theme={theme}>
       <div css={{ maxWidth: 960, margin: '0 auto' }}>
+        <Navigation />
         <main>{children}</main>
       </div>
     </ThemeProvider>
