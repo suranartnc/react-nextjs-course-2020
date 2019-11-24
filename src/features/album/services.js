ea40b91ecafbf6259e246ff3d528f0da9206c030
Suranart Niamcome <suranartnc@gmail.com>
2019-11-24 14:42:59 +0700

    Remove prepareData() from services

--- src/features/album/services.js
+++ src/features/album/services.js
@@ -5,36 +5,5 @@ export function getNewReleases({ token, limit }) {
 }
 
 export function getAlbumById(id, { token }) {
-  return API.getAlbumById(id, { token }).then(prepareData)
-}
-
-function prepareData({
-  artists,
-  images,
-  name,
-  total_tracks: totalTracks,
-  tracks,
-  release_date: releaseDate,
-}) {
-  return {
-    title: name,
-    subTitle: artists[0].name,
-    bottomLine: `${releaseDate.split('-')[0]} • ${totalTracks} Tracks`,
-    image: images[0].url,
-    tracks: tracks.items.map(
-      ({
-        name: trackName,
-        artists,
-        preview_url: previewUrl,
-        duration_ms: durationMs,
-      }) => ({
-        name: trackName,
-        artist: artists[0].name,
-        album: name,
-        image: images[0].url,
-        previewUrl,
-        durationMs,
-      }),
-    ),
-  }
+  return API.getAlbumById(id, { token })
 }
--- src/features/playlist/repository.js
+++ src/features/playlist/repository.js
@@ -1,14 +1,5 @@
 import { fetchAPI } from '@lib/api'
 
-export function getPlaylists({ token }) {
-  return fetchAPI({
-    path: `/browse/featured-playlists/?country=TH&timestamp=${new Date()
-      .toISOString()
-      .slice(0, -5)}`,
-    token,
-  })
-}
-
 export function getPlaylistById(id, { token }) {
   return fetchAPI({
     path: `/playlists/${id}`,
