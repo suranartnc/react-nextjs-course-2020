bb1d81f0f558f3d8a7c2db8ea80c987d9f8b8413
Suranart Niamcome <suranartnc@gmail.com>
2019-11-24 16:47:21 +0700

    Add more props for react player

--- src/features/player/Player.js
+++ src/features/player/Player.js
@@ -7,7 +7,20 @@ function Player() {
   const playerStore = new PlayerStore()
   const { url, playing } = playerStore.nowPlaying
 
-  return <ReactPlayer css={{ display: 'none' }} playing={playing} url={url} />
+  return (
+    <ReactPlayer
+      css={{ display: 'none' }}
+      playing={playing}
+      url={url}
+      progressInterval={50}
+      volume={0.8}
+      muted={false}
+      onProgress={data => console.log('onProgress', data)}
+      onEnded={() => {
+        console.log('onEnded')
+      }}
+    />
+  )
 }
 
 export default Player
