7c6ac9c81848d8c6b5850dd29c19d6726ce179eb
Suranart Niamcome <suranartnc@gmail.com>
2019-11-24 15:11:02 +0700

    Extract some code to a utility

---
+++ src/features/player/utilities.js
@@ -0,0 +1,9 @@
+export function convertSecondsToMinutes(totalSeconds) {
+  totalSeconds = Math.round(totalSeconds)
+  const minutes = Math.floor(totalSeconds / 60)
+  const seconds = totalSeconds % 60
+
+  const addLeadingZero = number => `${number <= 9 ? '0' : ''}${number}`
+
+  return `${addLeadingZero(minutes)}:${addLeadingZero(seconds)}`
+}
